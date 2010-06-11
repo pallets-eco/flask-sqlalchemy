89cda8e0a6e0dfc6d40264db2631c709ca58f01a
Armin Ronacher <armin.ronacher@active-4.com>
2010-06-11 03:48:43 +0200

    HEAD is 0.7

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='Flask-SQLAlchemy',
-    version='0.6',
+    version='0.7',
     url='http://github.com/mitsuhiko/flask-sqlalchemy',
     license='BSD',
     author='Armin Ronacher',
