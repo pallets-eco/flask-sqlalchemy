334b296c5f1e66a9a443b55742b7f4b171cd51d2
Thadeus Nathanial Burgess <thadeusb@thadeusb-desktop.(none)>
2010-08-24 07:54:15 +0800

    Added documentation for session extensions

--- flaskext/sqlalchemy.py
+++ flaskext/sqlalchemy.py
@@ -467,11 +467,30 @@ class SQLAlchemy(object):
         class User(db.Model):
             username = db.Column(db.String(80), unique=True)
             pw_hash = db.Column(db.String(80))
+            
+    You may also define your own SessionExtension instances as well when
+    defining your SQLAlchemy class instance. You may pass your custom instances
+    to the `session_extensions` keyword. This can be either a single 
+    SessionExtension instance, or a list of SessionExtension instances. In the 
+    following use case we use the VersionedListener from the SQLAlchemy 
+    versioning examples.::
+    
+        from history_meta import VersionedMeta, VersionedListener
+        
+        app = Flask(__name__)
+        db = SQLAlchemy(app, session_extensions=[VersionedListener()])
+        
+        class User(db.Model):
+            __metaclass__ = VersionedMeta
+            username = db.Column(db.String(80), unique=True)
+            pw_hash = db.Column(db.String(80))
     """
 
-    def __init__(self, app=None, use_native_unicode=True, session_extensions=None):
+    def __init__(self, app=None, use_native_unicode=True, 
+                 session_extensions=None):
         self.use_native_unicode = use_native_unicode
-        self.session_extensions = to_list(session_extensions, []) + [_SignallingSessionExtension()]
+        self.session_extensions = to_list(session_extensions, []) + \
+                                  [_SignallingSessionExtension()]
         
         self.session = _create_scoped_session(self)
 
