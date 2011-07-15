946384cc94c122b31f5b68e3d39710c6801f0f32
Armin Ronacher <armin.ronacher@active-4.com>
2011-07-15 18:14:02 +0200

    Bump to 0.13

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='Flask-SQLAlchemy',
-    version='0.12',
+    version='0.13',
     url='http://github.com/mitsuhiko/flask-sqlalchemy',
     license='BSD',
     author='Armin Ronacher',
