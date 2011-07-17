f70ac0bfbe44b5c80ed2050c47237e10c8c4d747
Armin Ronacher <armin.ronacher@active-4.com>
2011-07-17 15:52:31 +0200

    This is 0.14

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='Flask-SQLAlchemy',
-    version='0.13',
+    version='0.14',
     url='http://github.com/mitsuhiko/flask-sqlalchemy',
     license='BSD',
     author='Armin Ronacher',
