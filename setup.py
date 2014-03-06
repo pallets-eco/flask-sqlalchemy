5b739ef5a7059d2bcebb2aa54a2be8c99c3bc0ca
Nar Kumar <neokya@gmail.com>
2014-03-06 14:39:14 +0530

    add Python 2.7 and 3.3 as supported version

--- setup.py
+++ setup.py
@@ -38,6 +38,8 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
