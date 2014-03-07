399d0aa2e90a1ee20184aad75658efb7eb33814e
Alex Gaynor <alex.gaynor@gmail.com>
2014-03-06 16:24:41 -0800

    Added trove classifiers showing versions supported

--- setup.py
+++ setup.py
@@ -40,5 +40,11 @@ setup(
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
     ]
 )
