d6e12608bd8db2b97298df28da299dd06fec8eb5
Alec Nikolas Reiter <alecreiter@gmail.com>
2017-06-12 21:33:42 -0400

    Merge session test files together

--- tests/test_sessions.py
+++ tests/test_sessions.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 
@@ -63,3 +65,38 @@ def test_insert_update_delete(db):
 
 def test_listen_to_session_event(db):
     sa.event.listen(db.session, 'after_commit', lambda session: None)
+
+
+# need to create an instance of the extension so it has the custom session class attached
+# ergo the todo model class needs to be implemented here as well
+def test_custom_session_class(app):
+    tattle = {}
+
+    class CustomSession(fsa.SignallingSession):
+        def commit(self):
+            super(CustomSession, self).commit()
+            tattle['committed'] = True
+
+    db = fsa.SQLAlchemy(app=app, session_options={'class_': CustomSession})
+
+    class Todo(db.Model):
+        __tablename__ = 'todos'
+        id = db.Column('todo_id', db.Integer, primary_key=True)
+        title = db.Column(db.String(60))
+        text = db.Column(db.String)
+        done = db.Column(db.Boolean)
+        pub_date = db.Column(db.DateTime)
+
+        def __init__(self, title, text):
+            self.title = title
+            self.text = text
+            self.done = False
+            self.pub_date = datetime.utcnow()
+
+    with app.app_context():
+        db.create_all()
+        db.session.add(Todo(title='test custom session', text='make tattle true on commit'))
+        db.session.commit()
+        db.drop_all()
+
+    assert tattle['committed']
