fb70af554c49bd2fb0e0f8fda58cc58adc8b5d29
David Baumgold <david@davidbaumgold.com>
2018-05-10 10:56:36 +0200

    Drop support for Python 2.6
    SQLAlchemy requires Python 2.7 or higher.

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
