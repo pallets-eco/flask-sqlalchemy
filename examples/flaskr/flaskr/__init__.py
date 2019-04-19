7ad5738044aafa44058045db6dfe25c5c7f35f56
David Lord <davidism@gmail.com>
2019-04-19 13:43:55 -0700

    adapt flaskr example to sqlalchemy

---
+++ examples/flaskr/flaskr/__init__.py
@@ -0,0 +1,66 @@
+import os
+
+import click
+from flask import Flask
+from flask.cli import with_appcontext
+from flask_sqlalchemy import SQLAlchemy
+
+__version__ = (1, 0, 0, "dev")
+
+db = SQLAlchemy()
+
+
+def create_app(test_config=None):
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__, instance_relative_config=True)
+
+    # some deploy systems set the database url in the environ
+    db_url = os.environ.get("DATABASE_URL")
+
+    if db_url is None:
+        # default to a sqlite database in the instance folder
+        db_url = "sqlite:///" + os.path.join(app.instance_path, "flaskr.sqlite")
+        # ensure the instance folder exists
+        os.makedirs(app.instance_path, exist_ok=True)
+
+    app.config.from_mapping(
+        # default secret that should be overridden in environ or config
+        SECRET_KEY=os.environ.get("SECRET_KEY", "dev"),
+        SQLALCHEMY_DATABASE_URI=db_url,
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.update(test_config)
+
+    # initialize Flask-SQLAlchemy and the init-db command
+    db.init_app(app)
+    app.cli.add_command(init_db_command)
+
+    # apply the blueprints to the app
+    from flaskr import auth, blog
+
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+
+    # make "index" point at "/", which is handled by "blog.index"
+    app.add_url_rule("/", endpoint="index")
+
+    return app
+
+
+def init_db():
+    db.drop_all()
+    db.create_all()
+
+
+@click.command("init-db")
+@with_appcontext
+def init_db_command():
+    """Clear existing data and create new tables."""
+    init_db()
+    click.echo("Initialized the database.")
---
+++ examples/flaskr/tests/test_init.py
@@ -0,0 +1,27 @@
+from flaskr import create_app
+
+
+def test_config():
+    """Test create_app without passing test config."""
+    assert not create_app().testing
+    assert create_app({"TESTING": True}).testing
+
+
+def test_db_url_environ(monkeypatch):
+    """Test DATABASE_URL environment variable."""
+    monkeypatch.setenv("DATABASE_URL", "sqlite:///environ")
+    app = create_app()
+    assert app.config["SQLALCHEMY_DATABASE_URI"] == "sqlite:///environ"
+
+
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+
+    def fake_init_db():
+        Recorder.called = True
+
+    monkeypatch.setattr("flaskr.init_db", fake_init_db)
+    result = runner.invoke(args=["init-db"])
+    assert "Initialized" in result.output
+    assert Recorder.called
