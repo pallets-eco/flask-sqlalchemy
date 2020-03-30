1b8511157c1c349cff9282fa58c507b8186e7ab4
Aaron Wolmutt <awolmutt17@gmail.com>
2020-03-30 16:38:00 -0500

    missing dependency when deploying to a clean environment.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     packages=["flask_sqlalchemy"],
     include_package_data=True,
     python_requires=">= 2.7, != 3.0.*, != 3.1.*, != 3.2.*, != 3.3.*, !=3.4.*",
-    install_requires=["Flask>=1.0.4", "SQLAlchemy>=1.2"],
+    install_requires=["Flask>=1.0.4", "SQLAlchemy>=1.2", "psycopg2>=2.8.4"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
