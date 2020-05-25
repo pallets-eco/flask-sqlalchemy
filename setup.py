c435edeed959435098a9fb27da5c28e15e448076
David Lord <davidism@gmail.com>
2020-05-25 14:44:46 -0700

    use setup.cfg for setup metadata

--- setup.py
+++ setup.py
@@ -1,43 +1,13 @@
-import io
 import re
 
-from setuptools import find_packages
 from setuptools import setup
 
-with open("README.rst", "rt", encoding="utf8") as f:
-    readme = f.read()
+with open("src/flask_sqlalchemy/__init__.py", encoding="utf8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
-with open("src/flask_sqlalchemy/__init__.py", "rt", encoding="utf8") as f:
-    version = re.search(r'__version__ = "(.*?)"', f.read(), re.M).group(1)
-
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="Flask-SQLAlchemy",
     version=version,
-    url="https://github.com/pallets/flask-sqlalchemy",
-    project_urls={
-        "Documentation": "https://flask-sqlalchemy.palletsprojects.com/",
-        "Code": "https://github.com/pallets/flask-sqlalchemy",
-        "Issue tracker": "https://github.com/pallets/flask-sqlalchemy/issues",
-    },
-    license="BSD-3-Clause",
-    author="Armin Ronacher",
-    author_email="armin.ronacher@active-4.com",
-    maintainer="Pallets",
-    maintainer_email="contact@palletsprojects.com",
-    description="Adds SQLAlchemy support to your Flask application.",
-    long_description=readme,
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    include_package_data=True,
-    python_requires=">= 3.6",
     install_requires=["Flask>=1.0.4", "SQLAlchemy>=1.2"],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-    ],
 )
