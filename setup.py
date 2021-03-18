8347222dd303799af6192fc4e3ac13b46b77d096
David Lord <davidism@gmail.com>
2021-03-18 07:55:59 -0700

    move version to setup.cfg

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-import re
-
 from setuptools import setup
 
-with open("src/flask_sqlalchemy/__init__.py", encoding="utf8") as f:
-    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
-
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
-setup(
-    name="Flask-SQLAlchemy",
-    version=version,
-    install_requires=["Flask>=1.0.4", "SQLAlchemy>=1.2"],
-)
+setup(name="Flask-SQLAlchemy", install_requires=["Flask>=1.0.4", "SQLAlchemy>=1.2"])
