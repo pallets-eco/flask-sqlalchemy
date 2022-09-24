acc4b982319935fd545047009ffb35e37502d0f3
David Lord <davidism@gmail.com>
2022-09-24 11:18:49 -0700

    update todo example

--- examples/todo/app.py
+++ examples/todo/app.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from datetime import timezone
 
 from flask import flash
 from flask import Flask
@@ -8,32 +9,33 @@ from flask import request
 from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
-app.config.from_pyfile("hello.cfg")
+app.secret_key = "Achee6phIexoh8dagiQuew0ephuga4Ih"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.sqlite"
 db = SQLAlchemy(app)
 
 
-class Todo(db.Model):
-    __tablename__ = "todos"
-    id = db.Column("todo_id", db.Integer, primary_key=True)
-    title = db.Column(db.String(60))
-    text = db.Column(db.String)
-    done = db.Column(db.Boolean)
-    pub_date = db.Column(db.DateTime)
+def now_utc():
+    return datetime.now(timezone.utc)
 
-    def __init__(self, title, text):
-        self.title = title
-        self.text = text
-        self.done = False
-        self.pub_date = datetime.utcnow()
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    text = db.Column(db.String, nullable=False)
+    done = db.Column(db.Boolean, nullable=False, default=False)
+    pub_date = db.Column(db.DateTime, nullable=False, default=now_utc)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def show_all():
-    return render_template(
-        "show_all.html", todos=Todo.query.order_by(Todo.pub_date.desc()).all()
-    )
+    select = db.select(Todo).order_by(Todo.pub_date.desc())
+    todos = db.session.execute(select).scalars()
+    return render_template("show_all.html", todos=todos)
 
 
 @app.route("/new", methods=["GET", "POST"])
@@ -44,22 +46,20 @@ def new():
         elif not request.form["text"]:
             flash("Text is required", "error")
         else:
-            todo = Todo(request.form["title"], request.form["text"])
+            todo = Todo(title=request.form["title"], text=request.form["text"])
             db.session.add(todo)
             db.session.commit()
             flash("Todo item was successfully created")
             return redirect(url_for("show_all"))
+
     return render_template("new.html")
 
 
 @app.route("/update", methods=["POST"])
 def update_done():
-    for todo in Todo.query.all():
+    for todo in db.session.execute(db.select(Todo)).scalars():
         todo.done = f"done.{todo.id}" in request.form
+
     flash("Updated status")
     db.session.commit()
     return redirect(url_for("show_all"))
-
-
-if __name__ == "__main__":
-    app.run()
