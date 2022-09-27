02a10f352dd5fe3b817e059619ab9980f4e1498a
David Lord <davidism@gmail.com>
2022-09-27 11:26:31 -0700

    use pyproject.toml and pdm

--- docs/make.bat
+++ docs/make.bat
@@ -5,7 +5,7 @@ pushd %~dp0
 REM Command file for Sphinx documentation
 
 if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
+	set SPHINXBUILD=pdm run sphinx-build
 )
 set SOURCEDIR=.
 set BUILDDIR=_build
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 
-# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+# Metadata goes in pyproject.toml. These are here for GitHub's dependency graph.
 setup(name="Flask-SQLAlchemy", install_requires=["Flask>=2.0", "SQLAlchemy>=1.4"])
