0a2af2fd9bee6dd7ccdb628caa065d30ddb14613
Zoupers <qy@zouper.cn>
2023-05-06 22:56:01 +0800

    undo the change to cli.py

--- src/flask_sqlalchemy/cli.py
+++ src/flask_sqlalchemy/cli.py
@@ -5,18 +5,12 @@ import typing as t
 from flask import current_app
 
 
-if t.TYPE_CHECKING:
-    from .extension import SQLAlchemy as SQLAlchemy
-else:
-    SQLAlchemy = t.Any
-
-
 def add_models_to_shell() -> dict[str, t.Any]:
     """Registered with :meth:`~flask.Flask.shell_context_processor` if
     ``add_models_to_shell`` is enabled. Adds the ``db`` instance and all model classes
     to ``flask shell``.
     """
-    db: SQLAlchemy = current_app.extensions["sqlalchemy"]
+    db = current_app.extensions["sqlalchemy"]
     out = {m.class_.__name__: m.class_ for m in db.Model._sa_registry.mappers}
     out["db"] = db
     return out
