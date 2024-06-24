4f316c459d478be2f95f08bc47ae6caa4497d0a3
Frank Yu <withyuxiaoy@gmail.com>
2024-06-25 00:19:00 +0800

    add sqlalchemy namespace to flask shell

--- src/flask_sqlalchemy/cli.py
+++ src/flask_sqlalchemy/cli.py
@@ -2,15 +2,17 @@ from __future__ import annotations
 
 import typing as t
 
+import sqlalchemy as sa
 from flask import current_app
 
 
 def add_models_to_shell() -> dict[str, t.Any]:
     """Registered with :meth:`~flask.Flask.shell_context_processor` if
     ``add_models_to_shell`` is enabled. Adds the ``db`` instance and all model classes
-    to ``flask shell``.
+    to ``flask shell``. Adds the ``sqlalchemy`` namespace as ``sa`` to ``flask shell``.
     """
     db = current_app.extensions["sqlalchemy"]
     out = {m.class_.__name__: m.class_ for m in db.Model._sa_registry.mappers}
+    out["sa"] = sa
     out["db"] = db
     return out
